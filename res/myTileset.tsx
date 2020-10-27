8cbcb1456052a5917e29701ccf8669d686baf44c
Mikhail Balykin <mikport26@gmail.com>
2020-10-27 23:03:22 +0300

    reorganised files, added new files

---
+++ res/myTileset.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="myTileset" tilewidth="50" tileheight="50" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="50" height="50" source="floor.png"/>
+ </tile>
+ <tile id="1">
+  <image width="50" height="50" source="stair_down.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="50" source="stair_up.png"/>
+ </tile>
+ <tile id="3">
+  <image width="50" height="50" source="door_opened.png"/>
+ </tile>
+ <tile id="4">
+  <image width="50" height="50" source="door_closed.png"/>
+ </tile>
+ <tile id="5">
+  <image width="50" height="50" source="wall.png"/>
+ </tile>
+</tileset>
--- src/TApplication.cpp
+++ src/TApplication.cpp
@@ -11,21 +11,29 @@ namespace MyGame {
     TApplication::~TApplication() = default;;
 
     void TApplication::Init() {
-        window = new sf::RenderWindow(sf::VideoMode(640, 640), "DungeonGame");
+        window = new sf::RenderWindow(sf::VideoMode(500, 500), "DungeonGame");
     }
 
     void TApplication::Run() {
         Clock clock;
-        float CurrentFrame=0;
+        float CurrentFrame = 0;
 
-        Texture texture;
-        texture.loadFromFile("../img/leha_tiles.png");
+        Texture texture1;
+        Texture texture2;
+        texture1.loadFromFile("../res/img/temik_tiles.png");
+        texture2.loadFromFile("../res/img/leha_tiles.png");
 
         Sprite sprite;
-        sprite.setTexture(texture);
+        sprite.setTexture(texture1);
         sprite.setTextureRect(IntRect(0, 0, 30, 50));
         sprite.setScale(2.0, 2.0);
         sprite.setPosition(50, 25);
+
+        Sprite sprite2;
+        sprite2.setTexture(texture2);
+        sprite2.setTextureRect(IntRect(0, 0, 30, 50));
+        sprite2.setScale(2.0, 2.0);
+        sprite2.setPosition(450, 25);
 
         while (window->isOpen()) {
             float time = clock.getElapsedTime().asMicroseconds();
@@ -38,29 +46,62 @@ namespace MyGame {
                     window->close();
             }
 
+//            if (sprite2.getPosition().x < 500) {
+//                sprite2.move(0.1 * time, 0);
+//            } else if (sprite2.getPosition().x==500) {
+//                sprite2.setPosition(490, 25);
+//            } else {
+//                sprite2.setPosition(40, 25);
+//            }
+
             if (Keyboard::isKeyPressed(Keyboard::Left)) {
-                CurrentFrame+=0.005*time;
-                if(CurrentFrame>4) CurrentFrame-=4;
-                sprite.setTextureRect(IntRect(30*int(CurrentFrame), 0, 30, 50));
+                CurrentFrame += 0.005 * time;
+                if (CurrentFrame > 4) CurrentFrame -= 4;
+                sprite.setTextureRect(IntRect(30 * int(CurrentFrame), 0, 30, 50));
                 sprite.setScale(-2, 2);
-                sprite.move(-0.1*time, 0);
+                sprite.move(-0.1 * time, 0);
             }
             if (Keyboard::isKeyPressed(Keyboard::Right)) {
-                CurrentFrame+=0.005*time;
-                if(CurrentFrame>4) CurrentFrame-=4;
-                sprite.setTextureRect(IntRect(30*int(CurrentFrame), 0, 30, 50));
+                CurrentFrame += 0.005 * time;
+                if (CurrentFrame > 4) CurrentFrame -= 4;
+                sprite.setTextureRect(IntRect(30 * int(CurrentFrame), 0, 30, 50));
                 sprite.setScale(2, 2);
-                sprite.move(0.1*time, 0);
+                sprite.move(0.1 * time, 0);
             }
             if (Keyboard::isKeyPressed(Keyboard::Up)) {
-                sprite.move(0, -0.1*time);
+                sprite.move(0, -0.1 * time);
             }
             if (Keyboard::isKeyPressed(Keyboard::Down)) {
-                sprite.move(0, 0.1*time);
+                sprite.move(0, 0.1 * time);
             }
+
+            if (Keyboard::isKeyPressed(Keyboard::A)) {
+                CurrentFrame += 0.005 * time;
+                if (CurrentFrame > 4) CurrentFrame -= 4;
+                sprite2.setTextureRect(IntRect(30 * int(CurrentFrame), 0, 30, 50));
+                sprite2.setScale(-2, 2);
+                sprite2.move(-0.1 * time, 0);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::D)) {
+                CurrentFrame += 0.005 * time;
+                if (CurrentFrame > 4) CurrentFrame -= 4;
+                sprite2.setTextureRect(IntRect(30 * int(CurrentFrame), 0, 30, 50));
+                sprite2.setScale(2, 2);
+                sprite2.move(0.1 * time, 0);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::W)) {
+                sprite2.move(0, -0.1 * time);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::S)) {
+                sprite2.move(0, 0.1 * time);
+            }
+
+
+
 
             window->clear(sf::Color::Black);
             window->draw(sprite);
+            window->draw(sprite2);
             window->display();
         }
     }
